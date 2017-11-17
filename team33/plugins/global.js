108d6d48224f5a1afb45b0803589fce9e5411c38
Desmond Leung <dleungtm@gmail.com>
2017-11-16 16:23:33 -0800

    Update Grid to support conditionally showing actionButton Added Dropdown as a global component... hopefully makes appointment creation easier Implemented appointment deletion

--- team33/plugins/global.js
+++ team33/plugins/global.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import Grid from '../components/Grid.vue'
+import Dropdown from '../components/Dropdown.vue'
 
 Vue.component('grid', Grid)
+Vue.component('dropdown', Dropdown)
