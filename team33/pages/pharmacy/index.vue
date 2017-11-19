d71a5a3b0408d0c5bb301892edb09e121ae2cf96
kotes95 <nick.chim95@gmail.com>
2017-11-19 00:47:05 -0800

    Added unfilled prescription summary grid, using group by query

--- team33/pages/pharmacy/index.vue
+++ team33/pages/pharmacy/index.vue
@@ -4,23 +4,42 @@
     <div class="content">
       <div class="subsection">
         <div style="margin: 25px 10px;">
+          <span class="subsection-title" style="vertical-align: middle;">Unfilled Prescriptions Summary</span>
+        </div>
+        <div v-if="summaryItems.length > 0">
+          <form id="search">
+            <i class="fa fa-search" aria-hidden="true"></i>
+            <input name="query" v-model="searchQuery1" >
+          </form>
+          <br>
+          <grid
+            :data="summaryItems"
+            :columns="summaryGridColumns"
+            :filter-key="searchQuery1">
+          </grid>
+        </div>
+        <div v-else>
+          <h5>No pending prescriptions to summarize.</h5>
+        </div>
+      </div>
+      <div class="subsection">
+        <div style="margin: 25px 10px;">
           <span class="subsection-title" style="vertical-align: middle;">Medication Inventory</span>
           <nuxt-link class="button--default" to="/pharmacy/add_medication">Add Medication</nuxt-link>
         </div>
         <div v-if="medications.length > 0">
           <form id="search">
             <i class="fa fa-search" aria-hidden="true"></i>
-            <input name="query" v-model="searchQuery" >
+            <input name="query" v-model="searchQuery2" >
           </form>
           <br>
           <grid
             :data="medications"
             :columns="gridColumns"
-            :filter-key="searchQuery"
+            :filter-key="searchQuery2"
             :hasAction1="updateActionEnabled"
             :buttonAction1="updateAction"
-            :buttonLabel1="buttonLabel1"
-          >
+            :buttonLabel1="buttonLabel1">
           </grid>
         </div>
         <h5 v-if="medications.length < 1">The pharmacy does not yet stock any medication.</h5>
@@ -35,7 +54,13 @@
   export default {
     data () {
       return {
-        searchQuery: '',
+        searchQuery1: '',
+        summaryItems: [],
+        summaryGridColumns: [
+          { key: 'count', displayName: 'Count' },
+          { key: 'name', displayName: 'Name' }
+        ],
+        searchQuery2: '',
         gridColumns: [
           { key: 'name', displayName: 'Name' },
           { key: 'inventory', displayName: 'Inventory Count' },
@@ -52,8 +77,11 @@
       }
     },
     mounted () {
-      axios.get('/api/medications/').then(response => {
+      axios.get('/api/medications').then(response => {
         this.medications = response.data
+      })
+      axios.get('/api/medications/pending_prescriptions').then(response => {
+        this.summaryItems = response.data
       })
     },
     methods: {
